2725f1c060ced7aa1a6e689f4393e5bb3ece197c
Lapis Lapis <Iel Villaruel>
2020-03-16 18:27:31 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    WelcomeComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
